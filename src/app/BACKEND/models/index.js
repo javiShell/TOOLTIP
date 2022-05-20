57ebe3866497fce9f0f7f29d4d8180c7a0db7fd0
Javi <javi1996javi@gmail.com>
2022-05-20 22:33:32 +0200

    final

--- src/app/BACKEND/models/index.js
+++ src/app/BACKEND/models/index.js
@@ -2,8 +2,8 @@
 const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize-oracle');
-const oracledb = require('oracledb');
-oracledb.initOracleClient({libDir: 'C:\\instantclient_21_3'});
+//const oracledb = require('oracledb');
+//oracledb.initOracleClient({libDir: 'C:\\instantclient_21_3'});
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')[env];
